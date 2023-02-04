1a6d4a4a924371f8eb9080d7cf098484684a99fb
PavelAltynnikov <Robenzon11@gmail.com>
2023-02-04 23:19:17 +0500

    update Character class

--- model.py
+++ model.py
@@ -187,23 +187,27 @@ class Room(IRoom):
 
 
 class Character(ICharacter):
-    def __init__(self, room: IRoom):
-        self._room = room
+    def __init__(self):
+        self._room = None
 
     def change_room(self, room: IRoom):
         self._room = room
 
     def try_to_go_up(self):
-        self._room.try_to_release_character_up(self)
+        if self._room:
+            self._room.try_to_release_character_up(self)
 
     def try_to_go_right(self):
-        self._room.try_to_release_character_right(self)
+        if self._room:
+            self._room.try_to_release_character_right(self)
 
     def try_to_go_down(self):
-        self._room.try_to_release_character_down(self)
+        if self._room:
+            self._room.try_to_release_character_down(self)
 
     def try_to_go_left(self):
-        self._room.try_to_release_character_left(self)
+        if self._room:
+            self._room.try_to_release_character_left(self)
 
 
 class Wall(Boundary):
