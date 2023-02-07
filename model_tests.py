1f99b9c323a6abfd39034be98328e86b48bacc83
PavelAltynnikov <robenzon11@gmail.com>
2023-02-08 00:55:39 +0400

    add model tests module

---
+++ model_tests.py
@@ -0,0 +1,42 @@
+import unittest
+
+from model import BoundaryGenerator
+
+
+class BoundaryGeneratorTests(unittest.TestCase):
+
+    def test_calculate_internal_boundaries_amount_1(self):
+        size = 1
+        answer = 0
+
+        self.assertEqual(
+            BoundaryGenerator._calculate_internal_boundaries_amount(size),
+            answer
+        )
+
+    def test_calculate_internal_boundaries_amount_2(self):
+        size = 2
+        answer = 4
+
+        self.assertEqual(
+            BoundaryGenerator._calculate_internal_boundaries_amount(size),
+            answer
+        )
+
+    def test_calculate_internal_boundaries_amount_3(self):
+        size = 3
+        answer = 12
+
+        self.assertEqual(
+            BoundaryGenerator._calculate_internal_boundaries_amount(size),
+            answer
+        )
+
+    def test_calculate_internal_boundaries_amount_10(self):
+        size = 10
+        answer = 180
+
+        self.assertEqual(
+            BoundaryGenerator._calculate_internal_boundaries_amount(size),
+            answer
+        )
