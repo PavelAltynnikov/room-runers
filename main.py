6e623d8e92353ad3e7c083731de90fad19f9cc33
PavelAltynnikov <robenzon11@gmail.com>
2023-02-11 00:16:44 +0400

    use new modules in main module

--- main.py
+++ main.py
@@ -1,44 +1,18 @@
 from model.game_objects import Character
 from model.level import Level
+from view import LevelView, Controller
 
 VERSION = "0.1.1"
 
 
 def main():
-    required_answers = "wasdq"
-
     size = 10
     character = Character()
+    controller = Controller(character)
     level = Level(size, character)
-    level.print()
 
-    while True:
-        answer = input(
-            "Куда передвинуть персонажа?\n"
-            "w. Вверх\n"
-            "d. Вправо\n"
-            "s. Вниз\n"
-            "a. Влево\n"
-            "q. Закрыть игру\n"
-            "Ответ буквой: "
-        )
-
-        if answer not in required_answers:
-            print(f"Ваш ответ не понятен, введите один из символов {required_answers}")
-            continue
-        if answer == "q":
-            break
-
-        if answer == "w":
-            character.try_to_go_up()
-        elif answer == "d":
-            character.try_to_go_right()
-        elif answer == "s":
-            character.try_to_go_down()
-        elif answer == "a":
-            character.try_to_go_left()
-
-        level.print()
+    w = LevelView(level, controller)
+    w.show()
 
 
 if __name__ == "__main__":
