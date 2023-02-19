272e6306fbc8390cf07450763d3b8e8913750da9
PavelAltynnikov <robenzon11@gmail.com>
2023-02-19 23:28:49 +0400

    user new game rules and level view in main module

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.model.game_objects import Character, GameRules
+from src.model.game_objects import Character, GameRules, Timer
 from src.model.level import Level
 from src.view import LevelView, Controller
 
@@ -9,7 +9,8 @@ def main():
     character_1 = Character("Ripley")
     character_2 = Character("Alien")
 
-    game_rules = GameRules(character_1, character_2)
+    game_timer = Timer(5)
+    game_rules = GameRules(game_timer, character_1, character_2)
 
     size = 10
     level = Level(size, character_1, character_2)
@@ -17,8 +18,9 @@ def main():
     controller_1 = Controller(character_1)
     controller_2 = Controller(character_2)
 
-    w = LevelView(level, controller_1, controller_2)
+    w = LevelView(level, game_timer, controller_1, controller_2)
     w.characters_encounter_delegate = game_rules.check_characters_encounter
+    w.game_times_up = game_rules.check_times_up
     w.show()
 
 
